523f8859501d8b363d0a2a8d8a96586415668e27
Kaniaraa <kaniaraa26@gmail.com>
2025-06-03 09:41:04 +0700

    update database

--- xplorea.sql
+++ xplorea.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Bulan Mei 2025 pada 11.50
--- Versi server: 10.4.27-MariaDB
--- Versi PHP: 7.4.33
+-- Generation Time: Jun 03, 2025 at 04:37 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,14 +24,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(10) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seniman`
+--
+
+CREATE TABLE `seniman` (
+  `id_artist` int(10) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `username` varchar(500) DEFAULT NULL,
+  `followers` int(10) DEFAULT NULL,
+  `status_verifikasi` varchar(100) DEFAULT NULL,
+  `foto_profil` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `Id_user` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
   `username` varchar(500) NOT NULL,
-  `password` varchar(15) NOT NULL
+  `password` varchar(15) NOT NULL,
+  `alamat` varchar(500) DEFAULT NULL,
+  `foto_profil` mediumblob DEFAULT NULL,
+  `tgl_lahir` date DEFAULT NULL,
+  `gender` enum('laki-laki','perempuan') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -39,20 +71,43 @@ CREATE TABLE `user` (
 --
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `seniman`
+--
+ALTER TABLE `seniman`
+  ADD PRIMARY KEY (`id_artist`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`Id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `Id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `seniman`
+--
+ALTER TABLE `seniman`
+  ADD CONSTRAINT `seniman_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`Id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
